fadb8e6c257451148c66595a1f5024650aa872a9
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-09-10 15:32:32 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ pages = {
         st.Page("home.py", title="Home", icon=":material/home:"),
         st.Page("about.py", title="About", icon=":material/local_library:")
     ],
-    "Chart elements" : [
+    "Releases" : [
         st.Page("content/page1.py", title="Page 1", icon=":material/nutrition:"),
         st.Page("content/page2.py", title="Page 1", icon=":material/handyman:"),
     ]
