2498ec2bc1674d35dfb5df20ad8220c9ff8b54db
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-09-10 16:07:11 -0700

    Update st_context.py

--- content/st_context.py
+++ content/st_context.py
@@ -2,5 +2,10 @@ import streamlit as st
 
 st.subheader("st.context")
 
+st.write("🍪 Introducing st.context to read headers and cookies!")
+
 with st.echo():
+  st.context.headers
+
+with st.cookies():
   st.context.headers
