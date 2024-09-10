1d8031f7c0fcea72d16343f96f8fa6156a9028ae
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-09-10 16:13:07 -0700

    Update st_context.py

--- content/st_context.py
+++ content/st_context.py
@@ -4,10 +4,12 @@ st.subheader("st.context")
 
 st.write("🍪 Introducing `st.context` to read headers and cookies!")
 
+st.write("**context.headers**")
 with st.echo():
   st.context.headers
 
 st.divider()
 
+st.write("**context.cookies**")
 with st.echo():
-  st.context.headers
+  st.context.cookies
