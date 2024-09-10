fc52a3cc2f6da09253edfc6740b9a5e38ff73c72
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-09-10 15:22:07 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,20 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+st.set_page_config(page_title="Streamlit Releases", page_icon="🗺️")
 
-st.write('Hello world!')
+st.title('🗺️ Streamlit Releases')
+
+
+pages = {
+    "Main" : [
+        st.Page("home.py", title="Home", icon=":material/home:"),
+        st.Page("about.py", title="About", icon=":material/local_library:")
+    ],
+    "Chart elements" : [
+        st.Page("content/page1.py", title="Page 1", icon=":material/nutrition:"),
+        st.Page("content/page2.py", title="Page 1", icon=":material/handyman:"),
+    ]
+}
+
+pg = st.navigation(pages)
+pg.run()
