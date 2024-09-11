1ece08878a30364b9c17a827740d807b48f3a63f
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-09-10 19:48:07 -0700

    Update home.py

--- home.py
+++ home.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.write('Home')
 
-st.page_link("http://www.google.com", label="Google", icon=":material/thumb_up:")
+st.page_link("http://www.google.com", label="Google", icon=":material/menu_book:")
