7a659cb7f3cbca5048c7d55b8c54cc0cd44bcf67
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-09-10 22:02:17 -0700

    Update about.py

--- about.py
+++ about.py
@@ -4,6 +4,5 @@ st.subheader("About")
 
 st.info("""
   This is an interactive app for exploring recently released Streamlit features. 
-
-        For more information, check out the Release"
+  For more information, check out the Release"
         """)
