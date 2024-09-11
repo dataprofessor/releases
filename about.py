87077a9c1ae2187902f61ddfeca933a0d51418ee
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-09-10 22:33:31 -0700

    Update about.py

--- about.py
+++ about.py
@@ -7,6 +7,9 @@ st.info("""
   For more information, check out the following links:
         """)
 
+st.write("### Streamlit open source")
 st.page_link("https://docs.streamlit.io/develop/quick-reference/release-notes", label="Release notes", icon=":material/checklist:")
 st.page_link("https://docs.streamlit.io/develop/quick-reference/prerelease", label="Pre-release features", icon=":material/preview:")
 st.page_link("https://roadmap.streamlit.app/", label="Streamlit roadmap", icon=":material/map:")
+
+st.write("### Streamlit in Snowflake")
